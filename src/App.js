4b43ec67098ca144589744b2b79b1f22a8ad3333
MuratY2 <murat.yazgan@ozu.edu.tr>
2024-10-14 17:03:33 +0300

    add antdesign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// File: src/App.js
+import React from 'react';
+import { Layout } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Content, Footer } = Layout;
 
-export default App;
+const MainPage = () => {
+  return (
+    <Layout>
+      {/* Main Content */}
+      <Content className="simple-content">
+        <h1>Review Website</h1>
+      </Content>
+
+      {/* Footer */}
+      <Footer className="simple-footer">
+        Review ©2024
+      </Footer>
+    </Layout>
+  );
+};
+
+export default MainPage;
