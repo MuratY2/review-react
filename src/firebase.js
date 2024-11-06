0526bde35cf45042b015f25c544d7a1f80c6e95c
MuratY2 <murat.yazgan@ozu.edu.tr>
2024-11-06 19:43:36 +0300

    added axios for get

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';   
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDyWt_ZP3oVVsX1IVxypAfdoiB9sjbkNxU",
@@ -15,3 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);   
+export const db = getFirestore(app);
